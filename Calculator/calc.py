132decd603e2e80a667e45b5ef1a8d4b68e63487
markthecoderguy <139384872+markthecoderguy@users.noreply.github.com>
2024-11-08 08:18:46 +0000

    Add files via upload

---
+++ Calculator/calc.py
@@ -0,0 +1,30 @@
+import math
+num1 = int(input("number:\n  "))
+option = int(input("operation:\n1 for +\n2 for -\n3 for *\n4 for /\n5 for ²(square) \n6 for √(square root)\n7 for circumference of a circle(will work with diameter if chosen)\n    "))
+if option == 5:
+    print( float(num1 * num1) )
+elif option == 6:
+    print(math.sqrt(num1))
+elif option == 7:
+    print (float(num1) * (3.141592653589793238462643))
+
+else:
+    num2 = int(input("number:\n  "))
+
+    if option == 1:
+        print(num1 + num2)
+    
+    elif option == 2:
+        print (float(num1 - num2))
+
+    elif option == 3:
+        print (float(num1 * num2))
+
+    elif option == 4:
+        print (float(num1 / num2))
+    
+    else:
+        print("error")
+
+
+    
